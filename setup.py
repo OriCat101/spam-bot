317b73fcfe5454b6e4903636a954fdda7bd48543
HirushaPramuditha <hirushapramuditha2006@gmail.com>
2021-03-15 16:55:07 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="spam-bot",
-    version="0.0.4",
+    version="0.0.1",
     author="Hirusha Pramuditha",
     author_email="hirushapramuditha26@gmail.com",
     description="Spam anything!",
